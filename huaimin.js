fd0448d0551aa76bdc0781d6f0ae10e50477a27e
Huaimin <112915548+TeAmo-Lemon@users.noreply.github.com>
2025-01-06 19:18:05 +0800

    Update huaimin.js

--- huaimin.js
+++ huaimin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         循环搜索脚本 - 随机搜索问题
 // @namespace    http://tampermonkey.net/
-// @version      2025-01-06
+// @version      v0.2
 // @description  随机从预定义的问题列表中选择问题进行搜索，搜索后清空内容并刷新页面
 // @author       Huaimin
 // @match        https://*.bing.com/*
